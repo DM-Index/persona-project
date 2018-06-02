c88df77476ad13394b59eec74a7b802feb3807a0
Nathan <n.hatz@yahoo.com>
2018-06-02 14:55:41 -0500

    server & react responsive

--- server/index.js
+++ server/index.js
@@ -3,11 +3,12 @@ const { json } = require("body-parser");
 const cors = require("cors");
 // App server binding.
 const app = express();
-
+// middleware binding
 app.use(json());
 app.use(cors());
-
+// port declaration
 const port = 4000;
+// listener post
 app.listen(port, () => {
   console.log(`${port} Listening`);
 });
