5b1b5694e48b9f1ae9a49e1067636076533c3d6b
Nathan <n.hatz@yahoo.com>
2018-06-04 12:26:34 -0500

    Changes

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const { json } = require("body-parser");
 const cors = require("cors");
+const massive = require("massive");
 // App server binding.
 const app = express();
+// DB binding
+const port = process.env.PORT || 4001;
+const { CONNECTION_STRING } = process.env;
+
+massive(CONNECTION_STRING)
+  .then(db => {
+    app.set("dbInstance", db);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 // middleware binding
 app.use(json());
 app.use(cors());
 // port declaration
-const port = 4000;
 // listener post
 app.listen(port, () => {
   console.log(`${port} Listening`);
